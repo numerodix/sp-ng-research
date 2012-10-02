0caf0e9644c6b1a64ab01ce081d956b79f36a128
Martin Matusiak <numerodix@gmail.com>
2012-10-02 22:09:36 +0200

    add nested fk

--- datastore/sqlite.py
+++ datastore/sqlite.py
@@ -2,11 +2,12 @@
 
 import hashlib
 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.orm import sessionmaker
-
 
 Base = declarative_base()
 
@@ -17,9 +18,18 @@ class Url(Base):
     hash = Column(String)
     url = Column(String)
 
-    def __init__(self, url):
+    parent_id = Column(Integer, ForeignKey('urls.id'))
+    children = relationship(
+        'Url',
+        backref=backref('parent', remote_side=[id])
+    )
+
+    def __init__(self, url, children=None):
         self.url = url
         self.hash = hashlib.md5(url).hexdigest()
+
+        children = children and children or []
+        self.children = children
 
     def __repr__(self):
         return "<Url('%s', '%s')>" % (self.hash, self.url)
@@ -33,13 +43,30 @@ def main():
 
     Base.metadata.create_all(engine)
 
-    url = Url('http://docs.sqlalchemy.org/en/rel_0_7/orm/tutorial.html')
-    session.add(url)
+    demoset = {
+        'http://larrythecow.org/universe/':
+        [
+            'http://steveno.wordpress.com/2012/10/01/capture-2/',
+            'http://blog.siphos.be/2012/09/git-patch-apply/',
+        ],
+        'http://planetkde.org/':
+        [
+            'http://www.sinny.in/node/24',
+            'http://freininghaus.wordpress.com/2012/10/02/dolphin-bug-fixes-in-kde-4-9-2/',
+        ],
+    }
+
+    for parent, children in demoset.items():
+        ch = [Url(c) for c in children]
+        url = Url(parent, children=ch)
+        session.add(url)
 
     session.commit()
 
     for url in session.query(Url).all():
         print url
+        for i, ch in enumerate(url.children, 1):
+            print ' ', i, ch
 
 
 if __name__ == '__main__':
