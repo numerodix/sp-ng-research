7fa134500fa9bca7f6675d12b4bdb7b7ef447910
Martin Matusiak <numerodix@gmail.com>
2012-10-04 23:28:26 +0200

    count hits

--- frontend/web.py
+++ frontend/web.py
@@ -17,22 +17,29 @@ def get_package_path():
     return os.path.dirname(os.path.abspath(__file__))
 
 class Root(resource.Resource):
+    hits = 0
+
     def __init__(self):
         resource.Resource.__init__(self)
-        self.putChild('', Home())
+        self.putChild('', Home(self))
 
     def getChild(self, name, request):
+        self.hits += 1
         if request.uri.startswith('/static/'):
             fp = os.path.join(get_package_path(), 'static')
             return static.File(fp)
-        return resource.ErrorPage(404, 'Not found', 'Dunno where I put it')
+        return resource.ErrorPage(404, '404', 'Not found')
 
 class Home(resource.Resource):
-    numberRequests = 0
+    pageviews = 0
+
+    def __init__(self, root):
+        self.root = root
 
     def render_GET(self, request):
-        self.numberRequests += 1
-        msg = "I am request #%s\n" % self.numberRequests
+        self.pageviews += 1
+        self.root.hits += 1
+        msg = "Page views: %s, Hits: %s\n" % (self.pageviews, self.root.hits)
 
         path = os.path.join(get_package_path(), 'templates')
         loader = FileSystemLoader(path)
