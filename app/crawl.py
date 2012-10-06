7cdbdd7cccb001d73bba090156e4589bb6e565f1
Martin Matusiak <numerodix@gmail.com>
2012-10-07 00:47:59 +0200

    remove fragment

--- app/crawl.py
+++ app/crawl.py
@@ -27,6 +27,7 @@ def urljoin(url, path):
     if path:
         if not path.startswith('http'):
             url_new = urlparse.urljoin(url, path)
+            url_new = re.sub('#.*$', '', url_new)  # remove fragment
             url_new = re.sub('[.]{2}\/', '', url_new)
     return url_new
 
